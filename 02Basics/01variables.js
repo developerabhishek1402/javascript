45455b196fa174256e6a2f861c70c96de523013d
Abhishek C. Pathak <abhishek.pathak@digi-corp.com>
2022-05-07 00:49:57 +0530

    NEW: Variables and Data Types

---
+++ 02Basics/01variables.js
@@ -0,0 +1,18 @@
+
+//Stored Strings
+var fullName = 'Abhishek Pathak';
+
+var languageName = "JavaScript";
+
+//Stored Boolean
+var isLoggedIn = false;
+
+//Stored Numbers
+var loggedCount = 1;
+
+//Printing the value on console
+console.log(loggedCount);
+
+var paymentMode;
+
+console.log(paymentMode);
