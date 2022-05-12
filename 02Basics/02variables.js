ef08f25b9f59d98bb5cd3d65cda6a4a05d862f71
Abhishek C. Pathak <abhishek.pathak@digi-corp.com>
2022-05-13 00:21:22 +0530

    NEW: Completed the assignment

---
+++ 02Basics/02variables.js
@@ -0,0 +1,26 @@
+const uid = "xrt123";
+
+var firstName = "Abhishek";
+var lastName = "Pathak";
+var email = "developer.abhishek.1402@gmail.com";
+var password = "password";
+var confirmPassword = "password";
+var city = "Ahmedabad";
+var state = "Gujarat";
+var country = "India";
+var isLoggedInFromGoogle = false;
+var courseCount = 0;
+
+
+console.log(`
+
+    User unique id is : ${uid}
+    First name is : ${firstName}
+    Last name is : ${lastName}
+    Email of the user ${firstName} ${lastName} is ${email}
+    ${firstName} ${lastName} is from ${city}, ${state}, ${country}
+    and he is new to LearnCodeOnline. So he is enrolled in ${courseCount} course.
+    
+
+
+`)
