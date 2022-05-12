a5bd5cb143ce4d8338df4b9f48e7709912c3d04b
Abhishek C. Pathak <abhishek.pathak@digi-corp.com>
2022-05-13 00:35:44 +0530

    NEW: Completed and learned about the operators and math floor

---
+++ 02Basics/03operators.js
@@ -0,0 +1,12 @@
+var sellingPrice = 199;
+var listingPrice = 799;
+var courseName = "Angular Bootcamp";
+
+var discountPercentage = ((listingPrice - sellingPrice) / listingPrice) * 100;
+
+
+
+displayDiscountPercentage = Math.round(discountPercentage);
+
+console.log(`The total discount on the ${courseName} is ${displayDiscountPercentage}% `);
+
