cc5297810a43caeb104109cb762b81a7d2260455
Abhishek C. Pathak <abhishek.pathak@digi-corp.com>
2022-05-13 00:55:54 +0530

    NEW : Learned about the conditionals operators

---
+++ 02Basics/04conditionals.js
@@ -0,0 +1,13 @@
+var temprature = 39;
+
+//Get the temp data using the api
+
+if (temprature < 21) {
+    console.log(`It's cold outside`);
+}
+
+if(temprature < 30){
+    console.log(`It's moderate outside`);
+} else {
+    console.log(`It's really HOT outside`);
+}
