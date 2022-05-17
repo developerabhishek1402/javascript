669c6fd7a312e034e75b750ac167bf6f8d4267c3
Abhishek C. Pathak <abhishek.pathak@digi-corp.com>
2022-05-18 00:36:15 +0530

    NEW: Learn type coersion and falsy

---
+++ 02Basics/09truthyandfalsy.js
@@ -0,0 +1,18 @@
+//TODO Falsy
+
+//undefiend
+//null
+//0
+//''
+//Nan
+
+var user = "2";
+
+if(2 == user){
+    console.log('User is true')
+}
+
+
+if(2 === user){
+    console.log('User is true')
+}
